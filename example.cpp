f2bd3d151e1143419040a80ef48a51f2718b4253
longwatermelon <longwatermelon1@gmail.com>
2022-07-30 15:28:10 -0700

    simplify data

--- example.cpp
+++ example.cpp
@@ -8,14 +8,16 @@ int main()
     /* mt::io::print_vec(mt::vec3(1.f, 2.f, 3.f).cross(mt::vec3(2.f, 5.f, 3.f))); */
     /* mt::io::print_vec(mt::vec3(1.f, 5.f, 1.f).normalize()); */
 
-    mt::mat_data<float, 3> data = {
-        std::array<float, 3>{ 1.f, 2.f, 3.f },
-        std::array<float, 3>{ 4.f, 5.f, 6.f },
-        std::array<float, 3>{ 7.f, 8.f, 9.f }
-    };
+    mt::mat_data<float, 3> data = {{
+        { 1.f, 2.f, 3.f },
+        { 4.f, 5.f, 6.f },
+        { 7.f, 8.f, 9.f }
+    }};
 
     mt::mat3 m(data);
     mt::io::print_mat(m);
+
+    std::cout << "\n";
 
     mt::mat3 t = m.transpose();
     mt::io::print_mat(t);
