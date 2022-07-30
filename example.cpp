12d5b862d628ca075485828babd7550e4a9f5470
longwatermelon <longwatermelon1@gmail.com>
2022-07-30 15:40:13 -0700

    matrix multiply

--- example.cpp
+++ example.cpp
@@ -3,26 +3,43 @@
 
 int main()
 {
-    mt::vec3 v(1.f, 2.f, 3.f);
+    /* mt::vec3 v(1.f, 2.f, 3.f); */
 
     /* mt::io::print_vec(mt::vec3(1.f, 2.f, 3.f).cross(mt::vec3(2.f, 5.f, 3.f))); */
     /* mt::io::print_vec(mt::vec3(1.f, 5.f, 1.f).normalize()); */
 
-    mt::mat_data<float, 3> data = {{
-        { 1.f, 2.f, 3.f },
-        { 4.f, 5.f, 6.f },
-        { 7.f, 8.f, 9.f }
-    }};
+/*     mt::mat_data<float, 3> data = {{ */
+/*         { 1.f, 2.f, 3.f }, */
+/*         { 4.f, 5.f, 6.f }, */
+/*         { 7.f, 8.f, 9.f } */
+/*     }}; */
 
-    mt::mat3 m(data);
-    mt::io::print_mat(m);
+/*     mt::mat3 m(data); */
+/*     mt::io::print_mat(m); */
 
-    std::cout << "\n";
+/*     std::cout << "\n"; */
 
-    mt::mat3 t = m.transpose();
-    mt::io::print_mat(t);
+/*     mt::mat3 t = m.transpose(); */
+/*     mt::io::print_mat(t); */
 
     /* mt::mat4 mat(1.f); */
     /* mt::io::print_mat(mat); */
+
+    mt::mat_data<float, 3> d0 = {{
+        { -1.f, 1.f, -4.f },
+        { -3.f, 0.f, 5.f },
+        { 2.f, 4.f, 3.f }
+    }};
+
+    mt::mat_data<float, 3> d1 = {{
+        { 6.f, -1.f, 7.f },
+        { -2.f, 0.f, -3.f },
+        { 1.f, 2.f, 3.f }
+    }};
+
+    mt::mat3 m0(d0);
+    mt::mat3 m1(d1);
+
+    mt::io::print_mat(m0 * m1);
 }
 
--- include/mt/matrix.hpp
+++ include/mt/matrix.hpp
@@ -28,6 +28,24 @@ namespace mt
         mat(const mat_data<T, N> &data)
             : m_data(data) {}
 
+        mat<T, N> operator*(mat<T, N> m) const
+        {
+            mat_data<T, N> res;
+
+            for (std::size_t r = 0; r < N; ++r) // Row of this
+            {
+                for (std::size_t c = 0; c < N; ++c) // Column of m
+                {
+                    float f = m_data[r][0] * m.data()[0][c] +
+                              m_data[r][1] * m.data()[1][c] +
+                              m_data[r][2] * m.data()[2][c];
+                    res[r][c] = f;
+                }
+            }
+
+            return mat<T, N>(res);
+        }
+
         mat<T, N> transpose() const
         {
             mat_data<T, N> tr;
